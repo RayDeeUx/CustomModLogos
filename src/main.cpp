508f0faeefd94769e2bb9dd20efd9ff3f3bc8d55
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2025-08-07 10:09:28 -0400

    for the disbelievers

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,7 @@ $on_mod(Loaded) {
 	listenForSettingChanges("logging", [](bool newLogging) { Mod::get()->setLoggingEnabled(newLogging); });
 }
 // apparently this NEEDS to be in an execute thread (at least for v4.4.0)????? --raydeeux
-$execute {
+$on_mod(Loaded) {
 	new EventListener<EventFilter<ModLogoUIEvent>>(+[](ModLogoUIEvent* event) {
 		if (event->getModID().empty() || event->getModID() == "geode.loader") return ListenerResult::Propagate;
 		log::info("a new ModLogoUIEvent posted, LET'S BEGIN.");
